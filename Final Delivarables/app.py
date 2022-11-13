18122bc359fdf809222423b3075e4a75d487442b
Suriya-Prasad <prasad.suriya782@gmail.com>
2022-11-14 02:56:22 +0530

    updated app

--- Final Delivarables/app.py
+++ Final Delivarables/app.py
@@ -1,115 +1,147 @@
-import numpy as np
+# import numpy as np
 import os
-from tensorflow import keras
-from keras import models
-from keras.models import load_model
-from keras.preprocessing import image
-from keras.applications.inception_v3 import preprocess_input
 import requests
 from flask import Flask, request, render_template, redirect, url_for
 from cloudant.client import Cloudant
 
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+
+
 model = load_model(r"model\Updated-xception-diabetic-retinopathy.h5")
 
 app = Flask(__name__)
-# Authenticate using an IAM API key
+
+
+
+#Authenticate using an IAM API key
 client = Cloudant.iam('55a4f815-9a4a-4711-b663-d2733b89f3f9-bluemix','Ga7SGlD639xERt-F6egdft3j2dNntgT5CelqppKEgSLp', connect=True)
-# Create a database using an initialized client
+
+
+
+#create a database using an initialized client
 my_database = client.create_database('ibm-deeplearning')
-if my_database.exists():
-    print("Database '{0}' successfully created.".format('ibm-deeplearning'))
-# default home page or route
-@app.route('/')
-def index():
+
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+
+@app.route('/index')
+def home():
     return render_template('index.html')
 
-@ app.route('/index')
-def home():
-    return render_template("index.html")
 
-'''@ app.route('/register')
-def register():
-    return render_template("register.html")'''
+@app.route('/')
+def index():
+    return render_template('register.html')
 
-# registration page
-@ app.route('/register',methods=["GET","POST"])
+#registration page
+@app.route('/register')
 def register():
-    if request.method == "POST":
-        name =  request.form.get("name")
-        mail = request.form.get("emailid")
-        pswd = request.form.get("pass")
-        data = {
-            'name': name,
-            'mail': mail,
-            'psw': pswd
-        }
-        print(data)
-        query = {'mail': {'$eq': data['mail']}}
-        docs = my_database.get_query_result(query)
-        print(docs)
-        print(len(docs.all()))
-        if (len(docs.all()) == 0):
-            url = my_database.create_document(data)
-            return render_template("register.html", pred=" Registration Successful , please login using your details ")
-        else:
-            return render_template('register.html', pred=" You are already a member , please login using your details ")
+    return render_template('register.html')
+
+
+
+@app.route('/afterreg', methods=['POST'])
+def afterreg():
+
+    name = request.form.get('name')
+    email = request.form.get('emailid')
+    password = request.form.get('pass')
+
+    print(name,email,password)
+
+    data = {
+        '_id':email,
+        'name':name,
+        'psw':password,
+    }
+
+    print(data)
+
+    query = {'_id': {'$eq': data['_id']}}
+
+    docs = my_database.get_query_result(query)
+
+    print(docs)
+
+    print(len(docs.all()))
+
+    if(len(docs.all())==0):
+        url = my_database.create_document(data)
+        return render_template('register.html', pred="Registration successfull, Please login using your details")
     else:
-        return render_template('register.html')
-    
+        return render_template('register.html', pred="You are already a member, Please login using your details")
 
-@ app.route('/login', methods=['GET','POST'])
+
+
+#login page
+@app.route('/login')
 def login():
-    if request.method == "POST":
-        user = request.form.get('name')
-        passw = request.form.get('pass')
-        print(user, passw)
-        query = {'_id': {'$eq': user}}
-        docs = my_database.get_query_result(query)
-        print(docs)
-        print(len(docs.all()))
-        if (len(docs.all()) == 0):
-            return render_template('login.html', pred="The username is not found.")
-        else:
-            if ((user == docs[0][0]['_id'] and passw == docs[0][0]['pswd'])):
-                return redirect(url_for('prediction'))
-            else:
-                print('Invalid User')
-    else:
-        return render_template('login.html')
+    return render_template('login.html')
 
-@ app.route('/logout')
+
+
+@app.route('/afterlogin', methods=['POST'])
+def afterlogin():
+
+    user = request.form.get('emailid')
+    passw = request.form.get('pass')
+    print(user,passw)
+
+    query = {'_id': {'$eq': user}}
+
+    docs = my_database.get_query_result(query)
+    print(docs)
+
+    print(len(docs.all()))
+    if(len(docs.all())==0):
+        return render_template('login.html', pred="The username is not found, please Register")
+    else:
+        if((user==docs[0][0]['_id'] and passw==docs[0][0]['psw'])):
+            return render_template('index.html')
+        else:
+            print('Invalid User')
+
+
+@app.route('/logout')
 def logout():
     return render_template('logout.html')
 
-@app.route("/prediction")
-def predict():
-    return render_template("prediction.html")
 
-@ app.route('/result', methods=["GET", "POST"])
-def res():
-    if request.method == "POST":
-        f = request.files['image']
-        # getting the current path 1.e where app.py is present
-        basepath = os.path.dirname(__file__)
-        #print ( " current path " , basepath )
-        # from anywhere in the system we can give image but we want that
-        filepath = os.path.join(basepath, 'uploads', f.filename)
-        #print ( " upload folder is " , filepath )
-        f.save(filepath)
-        img = image.load_img(filepath, target_size=(299, 299))
-        x = image.img_to_array(img)  # ing to array
-        x = np.expand_dims(x, axis=0)  # used for adding one more dimension
-        #print ( x )
-        img_data = preprocess_input(x)
-        prediction = np.argmax(model.predict(img_data), axis=1)
-        # prediction = model.predict ( x ) #instead of predict_classes ( x ) we can use predict ( X ) ---- > predict_classes ( x ) gave error
-        # print ( " prediction is prediction )
-        index = [' No Diabetic Retinopathy ', ' Mild DR ',
-                 ' Moderate DR ', ' Severe DR ', ' Proliferative DR ']
-        # result = str ( index [ output [ 011 )
-        result = str(index[prediction[0]])
-        print(result)
-        return render_template('prediction.html', prediction=result)
+#prediction
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html',pred=None)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    f = request.files['image']
+    basepath = os.path.dirname(__file__) #getting the current path i.e. where app.py is present
+    filepath=os.path.join(basepath,'uploads',f.filename)
+    f.save(filepath)
+
+    img = image.load_img(filepath,target_size=(299,299))
+    x = image.img_to_array(img)
+    x=np.expand_dims(x,axis=0)#used for adding one more dimension
+    img_data=preprocess_input(x)
+    prediction=np.argmax(model.predict(img_data),axis=1)
+
+    print("prediction is", prediction)
+
+    index=['No Diabetic Retinopathy', 'Mild Diabetic Retinopathy', 'Moderate Diabetic Retinopathy', 'Severe Diabetic Retinopathy', 'Proliferative Diabetic Retinopathy']
+
+    res = str(index[prediction[0]])
+
+    return render_template('prediction.html',pred=res)
 
 if __name__ == "__main__":
     app.run(debug=False)
